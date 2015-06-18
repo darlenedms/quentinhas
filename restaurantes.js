383601d91754c83813076cc8946ed8cc5a082619
Bigode <bigode@corp.globo.com>
2015-06-18 14:32:46 -0300

    listagem de restaurantes

---
+++ restaurantes.js
@@ -0,0 +1,30 @@
+var restaurantes = [
+    {
+        "nome": "Miscelânea",
+        "telefone": "964089230",
+        "online": true,
+        "whatsapp": false,
+        "mediaPreco": 1, 
+    },
+    {
+        "nome": "Quentinha da Ana",
+        "telefone": "24948445",
+        "online": false,
+        "whatsapp": false,
+        "mediaPreco": 1,
+    },
+    {
+        "nome": "MegaArabe",
+        "telefone": "39822209",
+        "online": false,
+        "whatsapp": false,
+        "mediaPreco": 2,
+    },
+    {
+        "nome": "BioCarioca",
+        "telefone": "35980036",
+        "online": false,
+        "whatsapp": false,
+        "mediaPreco": 4,
+    },
+];
