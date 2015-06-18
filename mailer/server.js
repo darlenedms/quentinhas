4f9e2452f9ec3a975c45aeeddbeef6bbad997d7e
Daniel Filho <daniel.acfilho@gmail.com>
2015-06-18 17:10:02 -0300

    Criando um mailer de exemplo utilizando configurações do mailer_conf

--- mailer/server.js
+++ mailer/server.js
@@ -1,13 +1,39 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var nodemailer = require('nodemailer');
-var smtpTransport = require('nodemailer-smtp-transport');
+
+// Setup nconf
+var nconf = require("nconf");
+nconf.argv().env().file({file: 'mailer_conf.json'});
+
+// SMTP Server
+var nodemailer = require("nodemailer");
+var smtpTransport = require("nodemailer-smtp-transport");
+var transporter = nodemailer.createTransport(smtpTransport({
+  host: nconf.get("host"),
+  port: 25,
+  authMethod: "plain"
+}));
 
 var PORT = process.env.PORT || 3000;
 var PID = process.pid;
 
-app.get('/mailer', function (req, res) {
-  res.send('Hello World');
+app.get("/send", function (req, res) {
+  var mailOptions = {
+    from: nconf.get("from"),
+    to: nconf.get("to"),
+    subject: "hello world!",
+    text: "Authenticated with OAuth2"
+  };
+
+  transporter.sendMail(mailOptions, function(error, response) {
+    if (error) {
+      console.log(error);
+      res.end("error");
+    } else {
+      console.log("Message sent: " + response.message);
+      res.end("sent");
+    }
+  });
 });
 
 app.listen(PORT, function() {
